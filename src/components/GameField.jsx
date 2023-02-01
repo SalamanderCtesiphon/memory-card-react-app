89d564dc91aa6972a80f2af46d629924f8b080e9
SalamanderCtesiphon <swbrookshire@gmail.com>
2023-02-01 06:56:06 -0600

    created an array and dispayed it on the page using the map array method

--- src/components/GameField.jsx
+++ src/components/GameField.jsx
@@ -1,18 +1,16 @@
 const GameField = () => {
+    const gameArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+
     return (
         <div className="game-field">
-            <div className="card"></div>
-            <div className="card"></div>
-            <div className="card"></div>
-            <div className="card"></div>
-            <div className="card"></div>
-            <div className="card"></div>
-            <div className="card"></div>
-            <div className="card"></div>
-            <div className="card"></div>
-            <div className="card"></div>
-            <div className="card"></div>
-            <div className="card"></div>
+           {gameArray.map((i) => {
+            return (
+                <div 
+                key={gameArray.indexOf(i)}
+                className="card"
+                >{i}</div>
+            )
+           })}
         </div>
     )
 }
