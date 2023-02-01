ec275962bd21f223b2e97f5e778c8567e7317325
SalamanderCtesiphon <swbrookshire@gmail.com>
2023-02-01 07:10:43 -0600

    added a function to randomize the order of the array

--- src/components/GameField.jsx
+++ src/components/GameField.jsx
@@ -1,6 +1,12 @@
 const GameField = () => {
     const gameArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 
+    function shuffleArray(gameArray) {
+        gameArray.sort(() => Math.random() - 0.5);
+    }
+
+    shuffleArray(gameArray);
+
     return (
         <div className="game-field">
            {gameArray.map((i) => {
