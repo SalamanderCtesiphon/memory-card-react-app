e992a9a5f43511ac007a14e6623c563814076801
SalamanderCtesiphon <swbrookshire@gmail.com>
2023-02-01 06:12:58 -0600

    created a header

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+    return (
+        <div>
+            <h1>Memory Game</h1>
+            <p>Choose one card. If you choose the same card twice the game is over.</p>
+        </div>
+    )
+}
+
+export default Header
