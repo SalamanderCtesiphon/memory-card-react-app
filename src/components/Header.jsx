451a823302c58c1f30bce5ff8f38e9f74a5bc2de
SalamanderCtesiphon <swbrookshire@gmail.com>
2023-02-03 12:15:55 -0600

    still styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ const Header = ({ game, highScore }) => {
     return (
         <div className="header">
             <h1>Memory Game</h1>
-            <p>Choose one card. If you choose the same card twice the game is over.</p>
+            <div>Choose one card. If you choose the same card twice the game is over.</div>
             <p>Score: {game} | High Score: {highScore} </p>
         </div>
     )
