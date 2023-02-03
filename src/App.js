b84c027a4fe60f7880c68dcc6503d44161158c25
SalamanderCtesiphon <swbrookshire@gmail.com>
2023-02-03 11:33:21 -0600

    adding images

--- src/App.js
+++ src/App.js
@@ -4,6 +4,14 @@ import Header from './components/Header';
 import Card from './components/Card';
 import aNewHope from "./components/images/aNewHope.jpeg";
 import empire from "./components/images/empire.jpeg";
+import returnOfTheJedi from "./components/images/returnOfTheJedi.jpeg";
+import phantom from "./components/images/phantom.jpeg";
+import clones from "./components/images/clones.jpg";
+import sith from "./components/images/sith.jpeg";
+import forceAwakens from "./components/images/forceAwakens.jpg";
+import lastJedi from "./components/images/lastJedi.jpeg";
+import rise from "./components/images/rise.jpg";
+import rogue from "./components/images/rogue.jpeg"
 
 function App(props) {
   const [game, setGame] = useState(0);
@@ -22,34 +30,42 @@ function App(props) {
     {
         id: 3, 
         click: 0,
+        background: <img src={returnOfTheJedi} alt=" the return of the jedi" />,
     },
     {
         id: 4,
         click: 0,
+        background: <img src={phantom} alt=" the phantom menace" />,
     },
     {
         id: 5,
         click: 0,
+        background: <img src={clones} alt="attack of the clones" />,
     },
     {
         id: 6,
         click: 0,
+        background: <img src={sith} alt="revenge of the sith" />,
     },
     {
         id: 7,
         click: 0,
+        background: <img src={forceAwakens} alt="the force awakens" />,
     }, 
     {
         id: 8,
         click: 0,
+        background: <img src={lastJedi} alt="the last jedi" />,
     },
     {
         id: 9,
         click: 0,
+        background: <img src={rise} alt="the rise of skywalker" />,
     },
     {
         id: 10,
         click: 0,
+        background: <img src={rogue} alt="rogue" />,
     },
     {
         id: 11,
