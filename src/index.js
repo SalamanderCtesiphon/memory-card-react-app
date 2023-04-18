64f3c860d55f456f569605f19f74bbc9ff4f8003
salamanderctesiphon <swbrookshire@gmail.com>
2023-04-18 14:06:38 -0500

    cleaned up the file to deploy to firebase

--- src/index.js
+++ src/index.js
@@ -3,34 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { initializeApp } from 'firebase/app';
-import {
-  getAuth,
-  onAuthStateChanged,
-  GoogleAuthProvider,
-  signInWithPopup,
-  signOut,
-} from 'firebase/auth';
-import {
-  getFirestore,
-  collection,
-  addDoc,
-  query,
-  orderBy,
-  limit,
-  onSnapshot,
-  setDoc,
-  updateDoc,
-  doc,
-  serverTimestamp,
-} from 'firebase/firestore';
-import {
-  getStorage,
-  ref,
-  uploadBytesResumable,
-  getDownloadURL,
-} from 'firebase/storage';
-import { getMessaging, getToken, onMessage } from 'firebase/messaging';
-import { getPerformance } from 'firebase/performance';
+
 import { getFirebaseConfig } from './firebase-config.js';
 
 const firebaseAppConfig = getFirebaseConfig();
