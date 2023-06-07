14c1f4fc7718407c0fefe3c76c11314d8053b0a7
salamanderctesiphon <swbrookshire@gmail.com>
2023-06-07 10:40:44 -0500

    restyling the header moved elements around for a more appealing layout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,12 @@
 const Header = ({ game, highScore }) => {
     return (
         <div className="header">
-            <h1>Memory Game</h1>
+            <div className="top">
+                <h1>Memory Game</h1>
+                <h1>Score: {game} | High Score: {highScore}</h1>
+            </div>
             <div>Choose one card. If you choose the same card twice the game is over.</div>
-            <p>Score: {game} | High Score: {highScore} </p>
+            
         </div>
     )
 }
