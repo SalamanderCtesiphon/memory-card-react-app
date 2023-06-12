1ddf54e5ea5456399437682b3914a5e4d9b96bd8
salamanderctesiphon <swbrookshire@gmail.com>
2023-06-12 15:57:16 -0500

    commit

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,34 +1,19 @@
-/**
- * To find your Firebase config object:
- * 
- * 1. Go to your [Project settings in the Firebase console](https://console.firebase.google.com/project/_/settings/general/)
- * 2. In the "Your apps" card, select the nickname of the app for which you need a config object.
- * 3. Select Config from the Firebase SDK snippet pane.
- * 4. Copy the config object snippet, then add it here.
- */
-const config = {
-  apiKey: "AIzaSyCtoEVilsjmEX9xBv3qGmq6VZgIzD9cnGQ",
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-  authDomain: "memory-game-2198d.firebaseapp.com",
-
-  projectId: "memory-game-2198d",
-
-  storageBucket: "memory-game-2198d.appspot.com",
-
-  messagingSenderId: "631584871655",
-
-  appId: "1:631584871655:web:3acab5170ce6651a9efe42",
-
-  measurementId: "G-CPLE3L3ECM"
-
-
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDSlaaH_1W_jf0yiiQ5fMjNShuwrylN7rg",
+  authDomain: "memory-game-1f43b.firebaseapp.com",
+  projectId: "memory-game-1f43b",
+  storageBucket: "memory-game-1f43b.appspot.com",
+  messagingSenderId: "387844699737",
+  appId: "1:387844699737:web:a0b92367da9aa2c927e348"
 };
 
-export function getFirebaseConfig() {
-  if (!config || !config.apiKey) {
-    throw new Error('No Firebase configuration object provided.' + '\n' +
-    'Add your web app\'s configuration object to firebase-config.js');
-  } else {
-    return config;
-  }
-}
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app
